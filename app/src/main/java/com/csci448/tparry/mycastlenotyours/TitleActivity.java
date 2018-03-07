b90950de9a031b2d2f5b11690a412fe0bb548da7
tparry2 <tparry@mymail.mines.edu>
2018-03-06 19:44:26 -0700

    all files after Tuesday evening meeting

---
+++ app/src/main/java/com/csci448/tparry/mycastlenotyours/TitleActivity.java
@@ -0,0 +1,26 @@
+package com.csci448.tparry.mycastlenotyours;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class TitleActivity extends AppCompatActivity {
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+
+        startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), R.string.start_toast, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+    }
+}
