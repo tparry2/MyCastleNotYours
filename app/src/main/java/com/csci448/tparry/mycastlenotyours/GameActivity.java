d3acdaadc6a372febc518d9f29e29bb935e03a50
tparry2 <tparry@mymail.mines.edu>
2018-03-10 14:24:41 -0700

    Game activity and dialog boxes

---
+++ app/src/main/java/com/csci448/tparry/mycastlenotyours/GameActivity.java
@@ -0,0 +1,90 @@
+package com.csci448.tparry.mycastlenotyours;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class GameActivity extends AppCompatActivity {
+
+    private int TOTAL_HEALTH = 100;
+
+    private TextView mScoreTextView;
+    private int enemiesKilled = 0;
+
+    private ImageView mBowButton;
+    private TextView mHealthTextView;
+    private int healthRemaining = TOTAL_HEALTH;
+
+    private Button mGameOverButton;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        mScoreTextView = (TextView) findViewById(R.id.score_textview);
+        mScoreTextView.setText("Score: " + Integer.toString(enemiesKilled));
+
+        mBowButton = (ImageView) findViewById(R.id.arrow_button);
+        mBowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), R.string.bow_toast, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        mHealthTextView = (TextView) findViewById(R.id.health_textview);
+        mHealthTextView.setText("Health: " + Integer.toString(healthRemaining) + "/" + Integer.toString(TOTAL_HEALTH));
+
+        mGameOverButton = (Button) findViewById(R.id.game_over_button);
+        mGameOverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog highScoreDialog = onCreateHighScoreDialog(savedInstanceState);
+                highScoreDialog.show();
+
+                Dialog gameOverDialog = onCreateGameOverDialog(savedInstanceState);
+                gameOverDialog.show();
+            }
+        });
+
+    }
+
+
+    public Dialog onCreateHighScoreDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+        builder.setMessage("High Scores: "
+        + "\nYour Score: " + Integer.toString(enemiesKilled)
+        + "\nHigh Score: " + "9999999999")
+                .setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+    public Dialog onCreateGameOverDialog(final Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+        builder.setMessage(R.string.game_over)
+                .setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
--- app/src/main/java/com/csci448/tparry/mycastlenotyours/TitleActivity.java
+++ app/src/main/java/com/csci448/tparry/mycastlenotyours/TitleActivity.java
@@ -1,5 +1,9 @@
 package com.csci448.tparry.mycastlenotyours;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,20 +11,55 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class TitleActivity extends AppCompatActivity {
-    private Button startButton;
+    private Button mStartButton;
+    private Button mHighScoreButton;
+    private Button mExitButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
 
-        startButton = (Button) findViewById(R.id.start_button);
-        startButton.setOnClickListener(new View.OnClickListener() {
+        mStartButton = (Button) findViewById(R.id.start_button);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent i = new Intent(TitleActivity.this, GameActivity.class);
                 Toast toast = Toast.makeText(getApplicationContext(), R.string.start_toast, Toast.LENGTH_LONG);
                 toast.show();
+
+                startActivity(i);
             }
         });
+
+        mHighScoreButton = (Button) findViewById(R.id.high_score_button);
+        mHighScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog highScoreDialog = onCreateHighScoreDialog(savedInstanceState);
+                highScoreDialog.show();
+            }
+        });
+
+        mExitButton = (Button) findViewById(R.id.exit_button);
+        mExitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+
+    public Dialog onCreateHighScoreDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(TitleActivity.this);
+        builder.setMessage("High Score: "
+                + "\nHigh Score: " + "9999999999")
+                .setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {}
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
     }
 }
